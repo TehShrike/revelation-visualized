bdfcbc47835f3955271bd7043c216a5769153327
Josh Duff <me@JoshDuff.com>
2017-02-23 15:19:36 -0600

    It totally displays verses

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-var Ractive = require('ractive')
-var template = require('./revelation.html')
-var revelation = require('pickering-majority-text-revelation')
+const Ractive = require('ractive')
+const template = require('./revelation.html')
+const revelation = require('pickering-majority-text-revelation')
 
 
+const transformedVerses = revelation.versesNoteReferencesAndHeaders.map(function(verseChunk) {
+	if (verseChunk.type === 'verse' && !/^\u2014/.test(verseChunk.text)) {
+		verseChunk.text = ' ' + verseChunk.text
+	}
+	return verseChunk
+})
+
+const allStuff = []
+let currentParagraph = []
+let inParagraph = false
+
+transformedVerses.forEach(function(chunk) {
+	if (inParagraph) {
+		if (chunk.type === 'end paragraph') {
+			allStuff.push({
+				type: 'paragraph',
+				contents: currentParagraph
+			})
+			inParagraph = false
+			currentParagraph = null
+		} else {
+			currentParagraph.push(chunk)
+		}
+	} else if (chunk.type === 'start paragraph') {
+		currentParagraph = []
+		inParagraph = true
+	} else {
+		allStuff.push(chunk)
+	}
+})
+
+console.log(allStuff)
 
 new Ractive({
-	el: 'body',
+	el: '#verses',
 	template: template,
 	data: {
-		versesNoteReferencesAndHeaders: revelation.versesNoteReferencesAndHeaders,
+		versesNoteReferencesAndHeaders: allStuff,
 		notes: revelation.notes
 	}
 })
