eace97cc26538270878940c9c02c0df9b36f5d9c
Josh Duff <me@JoshDuff.com>
2017-02-25 15:56:00 -0600

    First swing at the structure of the book
    Based on PK's PDF

---
+++ structure.js
@@ -0,0 +1,180 @@
+module.exports = [
+	{
+		identifier: 'A',
+		title: 'Prologue',
+		description: 'How to Read This Book',
+		range: r([ 1, 1 ], [ 1, 11 ])
+	}, {
+		identifier: 'B',
+		title: 'First Septet – Seven Churches',
+		description: 'A Look at the Beginnings of the Church that Christ is Building',
+		range: r([ 1, 12 ], [ 3, 22 ]),
+		introduction: {
+			title: 'Introduction to the seven churches – Christ is present with His church',
+			range: r([ 1, 12 ], [ 1, 20 ])
+		},
+		subsections: makeSubsections(
+			s('Ephesus', r([ 2, 1 ], [ 2, 7 ])),
+			s('Smyrna', r([ 2, 8 ], [ 2, 11 ])),
+			s('Pergamos', r([ 2, 12 ], [ 2, 17 ])),
+			s('Thyatira', r([ 2, 18 ], [ 2, 29 ])),
+			s('Sardis', r([ 3, 1 ], [ 3, 6 ])),
+			s('Philadelphia', r([ 3, 7 ], [ 3, 13 ])),
+			s('Laodicea', r([ 3, 14 ], [ 3, 22 ]))
+		)
+	}, {
+		identifier: 'C',
+		title: 'Second Septet – Seven Seals',
+		description: 'Legal Judgments to be Executed Against Church’s Persecutors',
+		range: r([ 4, 1 ], [ 7, 17 ]),
+		introduction: {
+			title: 'Introduction to the seven seals – Christ is on His throne and is governing all of history',
+			range: r([ 4, 1 ], [ 5, 14 ])
+		},
+		subsections: [
+			s('Seal 1 - the white horse', r([ 6, 1 ], [ 6, 2 ]), 'a'),
+			s('Seal 2 - the red horse', r([ 6, 3 ], [ 6, 4 ]), 'b'),
+			s('Seal 3 - he black horse', r([ 6, 5 ], [ 6, 6 ]), 'c'),
+			s('Seal 4 - the yellowish-green horse', r([ 6, 7 ], [ 6, 8 ]), 'd'),
+			s('Seal 5 - the souls under the altar', r([ 6, 9 ], [ 6, 11 ]), 'e'),
+			s('Seal 6 - the earthquake', r([ 6, 12 ], [ 6, 17 ]), 'f'),
+			s('Interlude before the 7th seal: the 144,000 of the Jewish remnant and the innumerable multitude', r([ 7, 1 ], [ 7, 17 ])),
+			s('Seal 7 - introduces the seven trumpets and seems to comprise all of the third septet', r([ 8, 1 ], [ 8, 1 ]), 'g')
+		]
+	}, {
+		identifier: 'D',
+		title: 'Third Septet – Seven Trumpets',
+		description: 'The War Against the Church’s Persecutors',
+		range: r([ 8, 2 ], [ 11, 19 ]),
+		introduction: {
+			title: 'Introduction to the seven trumpets – God ordains victory for the church through prayer',
+			range: r([ 8, 2 ], [ 8, 6 ])
+		},
+		subsections: [
+			s('Trumpet 1 - The land is set on fire', r([ 8, 7 ], [ 8, 7 ]), 'a'),
+			s('Trumpet 2 - The sea is turned to blood', r([ 8, 8 ], [ 8, 9 ]), 'b'),
+			s('Trumpet 3 - The rivers and springs become bitter', r([ 8, 10 ], [ 8, 12 ]), 'c'),
+			s('Trumpet 4 - The heavenly bodies are dimmed', r([ 8, 12 ], [ 8, 13 ]), 'd'),
+			s('Trumpet 5 - Demons released from the pit', r([ 9, 1 ], [ 9, 12 ]), 'e'),
+			s('Trumpet 6 - Demons released from Euphrates', r([ 9, 13 ], [ 9, 21 ]), 'f'),
+			s('Interlude before 7th trumpet: The closing off of prophecy & the nature of prophecy', r([ 10, 1 ], [ 11, 14 ])),
+			s('Trumpet 7 - The seventh trumpet seems to comprise all of the fourth septet', r([ 11, 15 ], [ 11, 19 ]), 'g'),
+		]
+	}, {
+		identifier: 'E',
+		title: 'Fourth Septet – Seven Visions',
+		description: 'From Total Defeat to Victory',
+		range: r([ 12, 1 ], [ 15, 4 ]),
+		introduction: {
+			title: 'Introduction to the seven visions – The invisible battles are the key to the earthly ones',
+			range: r([ 12, 1 ], [ 12, 17 ]),
+			subsections: [
+				s('The Bride reflecting the glory of her husband', r([ 12, 1 ], [ 12, 1 ]), 'Ea'),
+				s('The Child of the woman', r([ 12, 2 ], [ 12, 2 ]), 'Eb'),
+				s('The Dragon tries to devour the Child', r([ 12, 3 ], [ 12, 5 ]), 'Ec'),
+				s('The woman flees to the wilderness', r([ 12, 6 ], [ 12, 6 ]), 'Ed'),
+				s('Dragon war in heaven', r([ 12, 7 ], [ 12, 9 ]), 'Ee'),
+				s('Victory of Christ & His people over the dragon', r([ 12, 10 ], [ 12, 11 ]), 'Ef'),
+				s('Dragon war on earth', r([ 12, 12 ], [ 12, 13 ]), 'Ee'),
+				s('The woman flees to the wilderness', r([ 12, 14 ], [ 12, 14 ]), 'Ed'),
+				s(`The Dragon's mouth & the earth swallows the serpents flood`, r([ 12, 15 ], [ 12, 16 ]), 'Ec'),
+				s('The rest of the offspring of the woman', r([ 12, 17, 0 ], [ 12, 17, 0 ]), 'Eb'),
+				s('The church reflecting the word of Christ', r([ 12, 17, 1 ], [ 12, 17 ]), 'Ea'),
+			]
+		},
+		subsections: [
+			s('The beast rising out of the sea', r([ 13, 1 ], [ 13, 10 ])),
+			s('The beast rising out of the land', r([ 13, 11 ], [ 13, 18 ])),
+			s('The 144,000 virgin (warriors) and the Lamb', r([ 14, 1 ], [ 14, 5 ])),
+			s('The seven angels', r([ 14, 6 ], [ 14, 13 ])),
+			s('The positive reaping of wheat', r([ 14, 14 ], [ 14, 16 ])),
+			s('The negative reaping of grapes', r([ 14, 17 ], [ 14, 20 ])),
+			s('The final "sign in heaven" seems to comprise everything in the fifth septet and guarantees the eventual conversion of all nations (15:1-4)', r([ 15, 1 ], [ 15, 1 ])),
+		]
+	}, {
+		identifier: 'D',
+		title: 'Fifth Septet – Seven Bowls of Wrath Containing the Seven Plagues',
+		range: r([ 15, 2 ], [ 16, 21 ]),
+		introduction: {
+			title: 'Introduction to the seven plagues – angels preparing for warfare; temple filled with God’s glory',
+			range: r([ 15, 2 ], [ 16, 1 ])
+		},
+		subsections: makeSubsections(
+			s('Bowl 1 - On the land', r([ 16, 2 ], [ 16, 2 ])),
+			s('Bowl 2 - On the sea', r([ 16, 3 ], [ 16, 3 ])),
+			s('Bowl 3 - On the waters', r([ 16, 4 ], [ 16, 7 ])),
+			s('Bowl 4 - On the sun', r([ 16, 8 ], [ 16, 9 ])),
+			s('Bowl 5 - On the throne of the beast', r([ 16, 10 ], [ 16, 11 ])),
+			s('Bowl 6 - On the Euphrates', r([ 16, 12 ], [ 16, 16 ])),
+			s('Bowl 7 - On the air – note that this 7th bowl seems to introduce all of the next septet (cf. 16:17-21)', r([ 16, 17 ], [ 16, 17 ])),
+		)
+	}, {
+		identifier: 'C',
+		title: 'Sixth Septet – Seven Condemnations of Babylon',
+		range: r([ 16, 17 ], [ 19, 5 ]),
+		introduction: {
+			title: 'Introduction to the seven condemnations – Even with Roman support, Jerusalem is no match for Christ',
+			range: r([ 16, 17 ], [ 16, 21 ])
+		},
+		subsections: makeSubsections(
+			s('Blasphemy of the Harlot', r([ 17, 1 ], [ 17, 6 ])),
+			s('Harlots Pagan Alliance with Rome', r([ 17, 7 ], [ 17, 18 ])),
+			s('Spiritual fornications', r([ 18, 1 ], [ 18, 8 ])),
+			s('Ungodly statist/commercial alliance', r([ 18, 9 ], [ 18, 20 ])),
+			s('The finality of Babylon’s fall', r([ 18, 21 ], [ 18, 24 ])),
+			s('All heaven agreeing with her judgment', r([ 19, 1 ], [ 19, 4 ])),
+			s('The death of the harlot is followed by the marriage of the Lamb', r([ 19, 5 ], [ 19, 10 ])),
+		)
+	}, {
+		identifier: 'B',
+		title: 'Seventh Septet – Seven visions of the victory of Christ’s Kingdom – The Church Militant & Triumphant',
+		range: r([ 19, 11 ], [ 22, 17 ]),
+		introduction: {
+			title: 'Introduction to the seven New Covenant visions – Jesus proves that He is King of kings and Lord of lords',
+			range: r([ 19, 11 ], [ 19, 21 ])
+		},
+		subsections: makeSubsections(
+			s('Satan’s power bound', r([ 20, 1 ], [ 20, 3 ])),
+			s('Victory over death guaranteed – reign in life and in death', r([ 20, 4 ], [ 20, 6 ])),
+			s('Final judgment', r([ 20, 11 ], [ 20, 15 ])),
+			s('All things made new', r([ 21, 1 ], [ 21, 8 ])),
+			s('The New Jerusalem as the spotless bride', r([ 21, 9 ], [ 21, 27 ])),
+			s('The river of life', r([ 22, 1 ], [ 22, 5 ])),
+			s('Reiteration that Christ will come soon to finish the old and to continue the renewal of all things', r([ 22, 6 ], [ 22, 17 ])),
+		)
+	}, {
+		identifier: 'A',
+		title: 'Epilogue: How to Read This Book',
+		range: r([ 22, 18 ], [ 22, 21 ])
+	}
+]
+
+const identifiers = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]
+function makeSubsections(...subsections) {
+	return subsections.map(({ title, range }, i) => {
+		return {
+			title,
+			range,
+			identifier: identifiers[i]
+		}
+	})
+}
+
+function s(title, range, identifier) {
+	return { title, range, identifier }
+}
+
+function r(rangeStart, randeEnd) {
+	return [
+		guaranteeRangeSection(rangeStart, 0),
+		guaranteeRangeSection(randeEnd, 9999),
+	]
+}
+
+function guaranteeRangeSection(range, defaultSection) {
+	if (range.length === 3) {
+		return range
+	} else {
+		return [ ...range, defaultSection ]
+	}
+}
---
+++ structured-text.js
@@ -0,0 +1,6 @@
+const revelation = require('pickering-majority-text-revelation')
+const oneToManyZip = require('one-to-many-array-zip')
+const withinRange = require('multi-part-range-compare')
+
+const structure = require('./structure')
+
