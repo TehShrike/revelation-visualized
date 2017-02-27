707868c695d14ea27375c226cf93466a90018436
Josh Duff <me@JoshDuff.com>
2017-02-27 16:47:48 -0600

    Now with paragraph breaks and chiasm colors on the side

--- index.js
+++ index.js
@@ -1,63 +1,112 @@
 const Ractive = require('ractive')
 const template = require('./revelation.html')
+const addVersesToStructure = require('./structured-text')
 const revelation = require('pickering-majority-text-revelation')
 const slugify = require('slugify')
 
-const transformedVerses = revelation.versesNoteReferencesAndHeaders.map(function(verseChunk) {
-	if (verseChunk.type === 'verse' && !/^\u2014/.test(verseChunk.text)) {
-		verseChunk.text = ' ' + verseChunk.text
+const verses = revelation.versesNoteReferencesAndHeaders
+.map(chunk => {
+	return chunk.type === 'end paragraph' ? { type: 'paragraph break' } : chunk
+})
+.filter(chunk => chunk.type !== 'start paragraph')
+.map(verseChunk => {
+	if (verseChunk.type !== 'verse' || /^\u2014/.test(verseChunk.text)) {
+		return verseChunk
 	}
-	return verseChunk
+
+	return Object.assign({}, verseChunk, { text: ' ' + verseChunk.text })
 })
 
-const allStuff = []
-let currentParagraph = []
-let inParagraph = false
+const structuredText = addVersesToStructure(verses)
 
-transformedVerses.forEach(function(chunk) {
-	if (inParagraph) {
-		if (chunk.type === 'end paragraph') {
-			allStuff.push({
-				type: 'paragraph',
-				contents: currentParagraph
-			})
-			inParagraph = false
-			currentParagraph = null
-		} else {
-			currentParagraph.push(chunk)
-		}
-	} else if (chunk.type === 'start paragraph') {
-		currentParagraph = []
-		inParagraph = true
-	} else {
-		allStuff.push(chunk)
-	}
-})
-
-// console.log(allStuff)
+console.log(structuredText)
 
 const colors = [
-	"#018d5d",
-	"#ba4460",
-	"#9ea946",
-	"#00479f",
-	"#c26939",
-	"#8188df",
-	"#ee6bd4"
+	'#018d5d',
+	'#ba4460',
+	'#9ea946',
+	'#00479f',
+	'#c26939',
+	'#8188df',
+	'#ee6bd4'
 ]
+
+const chiasmColors = {
+	a: 0,
+	b: 1,
+	c: 2,
+	d: 3,
+	e: 4,
+	f: 5,
+	g: 6,
+	h: 7
+}
+
+function getChiasmColor(identifier) {
+	const key = identifier[identifier.length - 1].toLowerCase()
+	const colorIndex = chiasmColors[key]
+	return colors[colorIndex]
+}
 
 function headerToSlug(header) {
 	return `header-${slugify(header.toLowerCase())}`
 }
 
+function splitIntoParagraphs(chunks) {
+	const paragraphs = []
+	let currentParagraph = []
+
+	function finishParagraph() {
+		paragraphs.push(currentParagraph)
+		currentParagraph = []
+	}
+
+	chunks.forEach(chunk => {
+		if (chunk.type === 'paragraph break') {
+			finishParagraph()
+		} else {
+			currentParagraph.push(chunk)
+		}
+	})
+
+	if (currentParagraph.length > 0) {
+		finishParagraph()
+	}
+
+	return paragraphs
+}
+
+const Paragraphs = Ractive.extend({
+	template: require('./paragraphs.html'),
+	twoway: false,
+	data: () => ({
+		headerToSlug,
+		colors
+	}),
+	computed: {
+		paragraphs() {
+			return splitIntoParagraphs(this.get('verses'))
+		}
+	}
+})
+
 new Ractive({
 	el: '#verses',
 	template: template,
 	data: {
-		versesNoteReferencesAndHeaders: allStuff,
-		notes: revelation.notes,
-		colors,
-		headerToSlug
+		structuredText,
+		getChiasmColor,
+		currentChiasm: null
+	},
+	components: {
+		Paragraphs
+	},
+	setChiasm(identifier) {
+		const currentChiasm = this.get('currentChiasm')
+
+		const newChiasm = currentChiasm === identifier ? null : identifier
+
+		this.set('currentChiasm', newChiasm)
 	}
 })
 
--- structured-text.js
+++ structured-text.js
@@ -1,53 +1,16 @@
-const revelation = require('pickering-majority-text-revelation')
 const oneToManyZip = require('one-to-many-array-zip')
 const withinRange = require('multi-part-range-compare')
 
 const structure = require('./structure')
 
-const verses = revelation.versesNoteReferencesAndHeaders.filter(chunk => chunk.type === 'verse')
+module.exports = function addVersesToStructure(verses) {
+	return oneToManyZip(structure, verses, ({ range }, verse) => {
+		const [ rangeStart, rangeEnd ] = range
 
-const sectionsWithVerses = oneToManyZip(structure, verses, ({ range }, verse) => {
-	const [ rangeStart, rangeEnd ] = range
-
-	return withinRange(rangeStart, rangeEnd, verseReference(verse))
-}).map(({ one: section, many: verses }) => Object.assign({}, section, { verses }))
-
-// printJson(sectionsWithVerses)
-module.exports = sectionsWithVerses
-
-// const next = sectionsWithVerses.map(section => {
-// 	return Object.assign({},
-// 		section,
-// 		forProperty(section, 'introduction', addVerses),
-// 		forProperty(section, 'subsections', (subsections, verses) => {
-// 			return subsections.map(subsection => addVerses(subsection, verses))
-// 		})
-// 	)
-// })
-
-// function addVerses(child, sectionVerses) {
-// 	const [ rangeStart, rangeEnd ] = child.range
-// 	const childVerses = sectionVerses.filter(verse => withinRange(rangeStart, rangeEnd, verseReference(verse)))
-// 	return Object.assign({}, child, { verses: childVerses })
-// }
-
-// function forProperty(section, property, transform) {
-// 	if (section[property]) {
-// 		return {
-// 			[property]: transform(section[property], section.verses)
-// 		}
-// 	}
-// }
-
-// printJson(next)
-
-
-
+		return verse.type !== 'verse' || withinRange(rangeStart, rangeEnd, verseReference(verse))
+	}).map(({ one: section, many: verses }) => Object.assign({}, section, { verses }))
+}
 
 function verseReference({ chapterNumber, verseNumber, sectionNumber }) {
 	return [ chapterNumber, verseNumber, sectionNumber ]
 }
-
-// function printJson(value) {
-// 	console.log(JSON.stringify(value, null, '\t'))
-// }
