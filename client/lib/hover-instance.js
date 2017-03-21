fb37647c9825c70339efe8da1a8ad4745dd254b6
Josh Duff <me@JoshDuff.com>
2017-03-21 18:25:19 -0500

    Highlight sermons when related verses are hovered

--- client/lib/hover-instance.js
+++ client/lib/hover-instance.js
@@ -1,7 +1,13 @@
 const withinRange = require('multi-part-range-compare')
+const createHoverInstance = require('lib/hover/index')
 
-function isInRange(targetVerse, range) {
-	return withinRange(range[0], range[1], targetVerse)
+function verseIsInSermonRange(targetVerse, sermonRange) {
+	return withinRange(sermonRange[0], sermonRange[1], targetVerse)
 }
 
-module.exports = require('lib/hover/index')(isInRange)
+function sermonContainsVerseRange(sermonRange, verseReference) {
+	return withinRange(sermonRange[0], sermonRange[1], verseReference)
+}
+
+module.exports.sermonHover = createHoverInstance(verseIsInSermonRange)
+module.exports.verseHover = createHoverInstance(sermonContainsVerseRange)
