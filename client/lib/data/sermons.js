45c32d899eead68cb6d0acd110fdcbc0a93b7a94
Josh Duff <me@JoshDuff.com>
2017-04-13 11:40:54 -0500

    Make the sermon modification immutable, just in case

--- client/lib/data/sermons.js
+++ client/lib/data/sermons.js
@@ -2,7 +2,7 @@ const sermons = require('lib/data/sermons.json')
 const guaranteeRangeSection = require('lib/guarantee-range-section')
 
 module.exports = sermons.map(sermon => {
-	return Object.assign(sermon, {
+	return Object.assign({}, sermon, {
 		range: [
 			guaranteeRangeSection(sermon.range[0]),
 			guaranteeRangeSection(sermon.range[1])
