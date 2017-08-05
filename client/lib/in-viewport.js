997b477e90d6c15b43bab026e679f679a5c07824
Josh Duff <me@JoshDuff.com>
2017-08-05 11:03:34 -0500

    Refactor in-viewport to make it more pleasing

--- client/lib/in-viewport.js
+++ client/lib/in-viewport.js
@@ -1,52 +1,48 @@
-var contains = function() {
+function contains() {
 	if (!global.document) {
-		return true;
+		throw new Error('NO GLOBAL DOCUMENT!')
 	}
-	return global.document.documentElement.compareDocumentPosition ?
-		function (a, b) {
-			return !!(a.compareDocumentPosition(b) & 16);
-		} :
-		global.document.documentElement.contains ?
-			function (a, b) {
-				return a !== b && ( a.contains ? a.contains(b) : false );
-			} :
-			function (a, b) {
-				while (b = b.parentNode) {
-					if (b === a) {
-						return true;
-					}
-				}
-				return false;
-			};
+
+	if (global.document.documentElement.compareDocumentPosition) {
+		return (container, element) => !!(container.compareDocumentPosition(element) & 16)
+	} else if (global.document.documentElement.contains) {
+		return (container, element) => container !== element && (container.contains ? container.contains(element) : false)
+	}
+
+	return function(container, element) {
+		while (element = element.parentNode) {
+			if (element === container) {
+				return true
+			}
+		}
+		return false
+	}
 }
 
-module.exports = function isVisible(elt, offset = 0) {
-	if (!contains(global.document.documentElement, elt)) {
-		return false;
+module.exports = function isVisible(element, offset = 0) {
+	if (!contains(global.document.documentElement, element)) {
+		return false
 	}
 
 	// Check if the element is visible
 	// https://github.com/jquery/jquery/blob/740e190223d19a114d5373758127285d14d6b71e/src/css/hiddenVisibleSelectors.js
-	// if (!elt.offsetWidth || !elt.offsetHeight) {
-	// 	return false;
+	// if (!element.offsetWidth || !element.offsetHeight) {
+	// 	return false
 	// }
 
-	var eltRect = elt.getBoundingClientRect();
-	var viewport = {
+	const elementRect = element.getBoundingClientRect()
+	const viewport = {
 		top: -offset,
 		left: -offset,
 		right: global.document.documentElement.clientWidth + offset,
 		bottom: global.document.documentElement.clientHeight + offset
-	};
+	}
 
 	// The element must overlap with the visible part of the viewport
-	var visible =
-		(
-			eltRect.right >= viewport.left &&
-			eltRect.left <= viewport.right &&
-			eltRect.bottom >= viewport.top &&
-			eltRect.top <= viewport.bottom
-		);
+	const visible = elementRect.right >= viewport.left
+		&& elementRect.left <= viewport.right
+		&& elementRect.bottom >= viewport.top
+		&& elementRect.top <= viewport.bottom
 
-	return visible;
+	return visible
 }
